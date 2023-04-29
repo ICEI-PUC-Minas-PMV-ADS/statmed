1775d63eef1086bee9dde40e0b7b47a5ba645e3d
Gabrielly Andrade <97044440+gabsandrade@users.noreply.github.com>
2023-04-29 16:57:51 -0300

    Att Controller

--- src/Statmed/Controllers/PacienteController.cs
+++ src/Statmed/Controllers/PacienteController.cs
@@ -14,6 +14,7 @@ namespace Statmed.Controllers
     public class PacienteController : ControllerBase
     {
         private static Paciente Nome = new Paciente();
+        [HttpGet]
         public IActionResult Get()
         {
             return Ok(Nome);
