69a0ffc3ea890bdefb1347a2a094a0b62a9f64f4
Diego <diegomjua@gmail.com>
2023-05-04 14:37:07 -0300

    Bug fix para alguns Paths e Readme

--- src/client/js/cid10.js
+++ src/client/js/cid10.js
@@ -1,7 +1,8 @@
-
 $(document).ready(function () {
     $('#tabela').DataTable({
         processing: true,
+        // Devido ao grande número de CIDs acredito que no futuro será melhor utilizar o backend para buscar, renderizar resultados e etc pois a DataTable carrega TODO banco de dados
+        //serverSide: true,
         language: {
             "lengthMenu": "Exibir _MENU_ registros por página",
             "search": "",
@@ -13,6 +14,7 @@ $(document).ready(function () {
             "infoFiltered": "(filtrado de _MAX_ regitros totais)"
             },
         ajax: {
+            // Trocar a URL pela API que será criada com o CID
             url: 'http://localhost:5145/api/Paciente/Consultar',
             type: 'GET',
             datatype: 'json',
@@ -24,6 +26,9 @@ $(document).ready(function () {
         columns: [
             { data: "idSame", title: "IdSame"},
             { data: "nome", title: "Nome"},
+            // Remover as duas acima e colocar as colunas CID abaixo
+            //{ data: "idCid", title: "CID"},
+            //{ data: "Desc", title: "Descrição"},
         ],
     });
 });
