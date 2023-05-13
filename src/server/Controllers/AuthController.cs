c1fbfba9f375d81bc625be6913db9055174db402
Gabrielly Andrade <gabriellycristiny16@gmail.com>
2023-05-13 16:03:44 -0300

    Api autenticação

--- src/server/Controllers/AuthController.cs
+++ src/server/Controllers/AuthController.cs
@@ -1,4 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Statmed.Models;
 
 namespace Statmed.Controllers
@@ -9,6 +13,12 @@ namespace Statmed.Controllers
     public class AuthController : ControllerBase
     {
         public static Usuario usuario = new Usuario();
+        private readonly IConfiguration _configuration; //trocar para a database (?)
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public ActionResult<Usuario> Register(UserDto request)
@@ -33,9 +43,31 @@ namespace Statmed.Controllers
             {
                 return BadRequest("Senha errada.");
             }
-                return Ok(usuario);
+                string token = CreateToken(usuario);
 
+                return Ok(token);
+        }
 
+        private string CreateToken(Usuario usuario)
+        {
+            List<Claim> claims = new List<Claim>{
+                new Claim(ClaimTypes.Name, usuario.Login)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        
         }
     }
 }
